10f0d7374da58cbd12e8db768f3a30dc1fc221b8
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-01-31 14:50:05 -0300

    Descrição breve do que foi feito no código

---
+++ Testando_Git_Repository/Testando_Git_Repository/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testando_Git_Repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O curso é brabo");
+        }
+    }
+}
